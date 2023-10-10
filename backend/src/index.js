eae5e342cf24739e56baa0fc2d6054c8624ce28d
matheusjardim-dev <matheusjardim.dev@gmail.com>
2023-10-09 21:29:47 -0300

    first route

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,4 +2,6 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => res.status(200).send('Hello World!'));
+
 app.listen(3333, () => console.log('Server running on port 3333'));
