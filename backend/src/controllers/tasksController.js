77b315c11c8dc91a8d440deb8302e137daf36963
matheusjardim-dev <matheusjardim.dev@gmail.com>
2023-10-11 14:32:36 -0300

    função createTask

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -1,7 +1,16 @@
-const getAll = (req, res) => {
-    return res.status(200).json({ message: 'tudo certo'})
+const tasksModel = require('../models/tasksModel');
+
+const getAll = async (_req, res) => {
+    const tasks = await tasksModel.getAll();
+    return res.status(200).json(tasks);
 };
 
+const createTask = async (req, res) => {
+    const createdTask = await tasksModel.createTask(req.body);
+    return res.status(201).json(createdTask);
+}
+
 module.exports = {
-    getAll
+    getAll,
+    createTask,
 }
--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -1,10 +1,22 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-    const tasks = await connection.execute('SELECT * FROM todolist.tasks');
+    const [tasks] = await connection.execute('SELECT * FROM todolist.tasks');
     return tasks;
 };
 
+const createTask = async (task) => {
+    const { title } = task;
+
+    const dateUTC = new Date(Date.now()).toUTCString();
+
+    const query = 'INSERT INTO todolist.tasks (title, status, created_at) VALUES (?, ?, ?)';
+    const [createdTask] = await connection.execute(query, [title, 'pendente', dateUTC]);
+
+    return {insertId: createdTask.insertId};
+}
+
 module.exports = {
-    getAll
+    getAll,
+    createTask,
 };
